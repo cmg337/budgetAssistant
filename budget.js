11614f613f37e13a2f96129c3a9c4fbb364ccfb9
Christopher Gorski <Christopher.Gorski@bd.com>
2019-08-19 18:37:02 -0400

    popup now saves changes to chrome storage

--- budget.js
+++ budget.js
@@ -1,3 +1,23 @@
-document.onload = function () {
+window.onload = function () {
 
+    chrome.storage.sync.get(['rent', 'income', 'savings'], function (result) {
+        for (var i = 0; i < 3; i++) {
+            document.getElementById(Object.keys(result)[i]).value = result[Object.keys(result)[i]]
+        }
+    });
+
+
+    document.getElementById("saveButton").addEventListener("click", function () {
+        chrome.storage.sync.set({ income: document.getElementById("income").value }, function () {
+            console.log("Income Changed");
+        });
+        chrome.storage.sync.set({ rent: document.getElementById("rent").value }, function () {
+            console.log("Rent Changed");
+        });
+        chrome.storage.sync.set({ savings: document.getElementById("savings").value }, function () {
+            console.log("Saving Goal Changed");
+        });
+        document.getElementById("container").appendChild(document.createElement("p")).textContent = "Saved"
+    })
 }
+    
