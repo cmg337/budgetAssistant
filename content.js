933123beeea857bc7a48f514c8cb87fc90343b77
Chris Gorski <c.gorski82@gmail.com>
2019-08-26 01:41:48 -0400

    refactor and fix issues with child nodes and multiple dollar values in one node

--- content.js
+++ content.js
@@ -10,36 +10,55 @@ convertMoney = function (elem) {
     var altRegex = /\$\s[0-9]+(\s[0-9][0-9])?/
     var altRegexAll = /\$\s[0-9]+(\s[0-9][0-9])?/g
     var badTagRegex = /img|script/i
-    var goodTagRegex = /span|strong/i
+    
+
     // if textContent matches regex - disclude containers of any kind scripts and images
-    if (dollarRegex.test(elem.innerText) && !badTagRegex.test(elem.tagName) && (goodTagRegex.test(elem.tagName) || elem.childElementCount == 0)) {
+    if ((dollarRegex.test(elem.innerText) || altRegex.test(elem.innerText)) && !badTagRegex.test(elem.tagName) && childrenAreText(elem)) {
+        var replacements = {};//use object to keep track of values if multiple
         // get price and convert to number
-        for ( var i in elem.innerText.match(dollarRegexAll)){
-            var cost = elem.innerText.match(dollarRegexAll)[i];
-            var costInt = cost.replace("$", "");
-            // sync.get needs is async and element needs to be changed inside method
-            chrome.storage.sync.get(['income'], function (result) {
+        if (dollarRegex.test(elem.innerText)) {
+            for (var i in elem.innerText.match(dollarRegexAll)) {//loop through all matches in element
+                var cost = elem.innerText.match(dollarRegexAll)[i];
+                var costInt = cost.replace("$", "") ;
+                replacements[cost] = costInt;
+            }
+        } else {
+            for (var i in elem.innerText.match(altRegexAll)) {//loop through all matches in element
+                var cost = elem.innerText.match(altRegexAll)[i];
+                var costInt = cost.replace("$", "").trim().replace(/\s/, ".");
+                replacements[cost] = costInt;
+            }
+        }
+        
+        // sync.get needs is async and element needs to be changed inside method
+        chrome.storage.sync.get(['income'], function (result) {
+            for (var i in Object.keys(replacements)) {
+                var cost = Object.keys(replacements)[i];
+                var costInt = replacements[cost];
                 var wage = result.income / WEEKS / HOURS;
                 var converted = (Math.floor(costInt / wage) + " hours, " + Math.floor(costInt % wage * 60 / wage) + " minutes");
                 elem.innerText = elem.innerText.replace(cost, converted);
-                })
             }
-        } else if(altRegex.test(elem.innerText) && !badTRegex.test(elem.tagName) && (goodTagRegex.test(elem.tagName) ||  elem.childElementCount == 0)) {
-            
-            // get price and convert to number
-            for ( var i in elem.innerText.match(altRegexAll)){
-                var cost = elem.innerText.match(altRegexAll)[i];
-                var costInt = cost.replace("$", "").trim().replace(/\s/, ".");
-                // sync.get needs is async and element needs to be changed inside method
-                chrome.storage.sync.get(['income'], function (result) {
-                    var wage = result.income / WEEKS / HOURS;
-                    var converted = (Math.floor(costInt / wage) + " hours, " + Math.floor(costInt % wage * 60 / wage) + " minutes");
-                    elem.innerText = elem.innerText.replace(cost, converted);
-                    })
-                }
-            }
-        }
+        })
+    } 
+}
 
+//checks if element inner text can be replaced based on child node tags
+const childrenAreText = function (elem) {
+    var goodTagRegex = /span|strong/i;
+    if (elem.childElementCount == 0) {//0 children good
+        return true
+    };
+    if (!goodTagRegex.test(elem.tagName)) {//must have good tag
+        return false
+    };
+
+    var checker = true;//set outside var to true -check children with foreach
+    elem.childNodes.forEach((child) => {//children have good tags
+        if (!goodTagRegex.test(child.tagName)) { checker = false }
+    })
+    return checker;
+}
 
 
 
